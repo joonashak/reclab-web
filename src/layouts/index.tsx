515ac6292993a8e3544b3db2e02fcdae5caaa486
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-17 22:19:18 +0300

    Add simple navbar

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,12 +1,52 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  AppBar,
+  Button,
+  IconButton,
+  makeStyles,
+  Toolbar,
+  Typography,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const Layout = ({ children }) => (
-  <div>
-    <h1>hi, layout</h1>
-    {children}
-  </div>
-);
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Layout = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar position="sticky">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Recover Laboratory
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      {children}
+    </div>
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
