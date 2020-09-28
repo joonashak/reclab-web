e81f2120d48c0efc19dfcb4e22d4ca1cce953f82
Joonas Häkkinen <joonas@joonashakkinen.fi>
2020-09-29 00:17:05 +0300

    Add prop types

--- src/templates/Page.tsx
+++ src/templates/Page.tsx
@@ -1,11 +1,21 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
-import { graphql } from 'gatsby';
+import PropTypes, { InferProps } from 'prop-types';
 import { Typography } from '@material-ui/core';
 
-export default ({ pageContext }) => (
+const Page = ({ pageContext }: InferProps<typeof Page.propTypes>) => (
   <>
     <Typography variant="h3">{pageContext.data.title}</Typography>
     <Typography>{pageContext.data.content}</Typography>
   </>
 );
+
+Page.propTypes = {
+  pageContext: PropTypes.shape({
+    data: PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default Page;
